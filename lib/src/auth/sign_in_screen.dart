3ba4a6936252f3694b738717aebab6def9c1846f
Estelson Medeiros Pereira <estelson@gmail.com>
2022-11-13 00:46:13 -0300

    Criando os botões de "Entrar" e "Esqueceu a senha?"

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 
@@ -18,27 +16,56 @@ class SignInScreen extends StatelessWidget {
         ),
         Expanded(
           child: Container(
-            padding: EdgeInsets.symmetric(
+            padding: const EdgeInsets.symmetric(
               horizontal: 32,
               vertical: 40,
             ),
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.vertical(top: Radius.circular(45)),
             ),
             child: Column(
-              children: const [
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
                 // Email
-                CustomTextField(
+                const CustomTextField(
                   icon: Icons.email,
                   label: "E-mail",
                 ),
 
                 // Senha
-                CustomTextField(
+                const CustomTextField(
                   icon: Icons.lock,
                   label: "Senha",
                   isSecret: true,
+                ),
+
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Entrar",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Esqueceu a senha?",
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
