f1e16890cf87dd850f09ad8477c9bcbf1244b3a3
Estelson Medeiros Pereira <estelson@gmail.com>
2022-11-14 03:41:09 -0300

    Adicionando o botão de voltar e aplicando máscaras
    - Utilização da lib "mask_text_input_formatter: ^2.4.0" para formatação dos campos Celular e CPF

--- lib/src/auth/components/sign_up_screen.dart
+++ lib/src/auth/components/sign_up_screen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({Key? key}) : super(key: key);
+  SignUpScreen({Key? key}) : super(key: key);
+
+  final phoneFormatter = MaskTextInputFormatter(
+    mask: "## # ####-####",
+    filter: {"#": RegExp(r'[0-9]')}
+  );
+
+  final cpfFormatter = MaskTextInputFormatter(
+    mask: "###.###.###-##",
+    filter: {"#": RegExp(r'[0-9]')}
+  );
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -16,85 +26,108 @@ class SignUpScreen extends StatelessWidget {
         child: SizedBox(
           height: size.height,
           width: size.width,
-          child: Column(
+          child: Stack(
             children: [
-              // Título da tela
-              const Expanded(
-                child: Center(
-                  child: Text(
-                    "Cadastro",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35,
-                    ),
-                  ),
-                ),
-              ),
-      
-              // Formulário da tela
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 40,
-                ),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(45),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    // E-mail
-                    const CustomTextField(
-                      icon: Icons.email,
-                      label: "E-mail",
-                    ),
-      
-                    // Senha
-                    const CustomTextField(
-                      icon: Icons.lock,
-                      label: "Senha",
-                      isSecret: true,
-                    ),
-      
-                    // Nome
-                    const CustomTextField(
-                      icon: Icons.person,
-                      label: "Nome",
-                    ),
-      
-                    // Celular
-                    const CustomTextField(
-                      icon: Icons.phone,
-                      label: "Celular",
-                    ),
-      
-                    // CPF
-                    const CustomTextField(
-                      icon: Icons.file_copy,
-                      label: "CPF",
-                    ),
-      
-                    // Botão Cadastrar usuário
-                    SizedBox(
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        )),
-                        onPressed: () {},
-                        child: const Text(
-                          "Cadastrar usuário",
-                          style: TextStyle(
-                            fontSize: 18,
-                          ),
+              Column(
+                children: [
+                  // Título da tela
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        "Cadastro",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
                         ),
                       ),
                     ),
-                  ],
+                  ),
+
+                  // Formulário da tela
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 40,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(45),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        // E-mail
+                        const CustomTextField(
+                          icon: Icons.email,
+                          label: "E-mail",
+                        ),
+
+                        // Senha
+                        const CustomTextField(
+                          icon: Icons.lock,
+                          label: "Senha",
+                          isSecret: true,
+                        ),
+
+                        // Nome
+                        const CustomTextField(
+                          icon: Icons.person,
+                          label: "Nome",
+                        ),
+
+                        // Celular
+                        CustomTextField(
+                          icon: Icons.phone,
+                          label: "Celular",
+                          inputFormatters: [phoneFormatter],
+                        ),
+
+                        // CPF
+                        CustomTextField(
+                          icon: Icons.file_copy,
+                          label: "CPF",
+                          inputFormatters: [cpfFormatter],
+                        ),
+
+                        // Botão Cadastrar usuário
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            )),
+                            onPressed: () {},
+                            child: const Text(
+                              "Cadastrar usuário",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              
+              Positioned(
+                left: 10,
+                top: 10,
+
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
             ],
