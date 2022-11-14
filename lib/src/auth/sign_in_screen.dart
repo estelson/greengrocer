fff8742bc2849e295f8e4860c3584bf2664a1f64
Estelson Medeiros Pereira <estelson@gmail.com>
2022-11-14 03:43:47 -0300

    Reorganização de códigos

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/auth/components/custom_text_field.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:greengrocer/src/auth/components/sign_up_screen.dart';
+import 'package:greengrocer/src/auth/sign_up_screen.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
