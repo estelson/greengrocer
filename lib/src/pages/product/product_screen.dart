b02a91754527ec50db5998d1b6ff56c428901923
Estelson Medeiros Pereira <estelson@gmail.com>
2022-11-14 23:15:27 -0300

    Adicionando os detalhes do produto ao container inferior

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
-  const ProductScreen({Key? key, required this.item}) : super(key: key);
+  ProductScreen({Key? key, required this.item}) : super(key: key);
 
   final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
 
   @override
   Widget build(BuildContext context) {
@@ -19,16 +22,74 @@ class ProductScreen extends StatelessWidget {
 
           Expanded(
             child: Container(
+              padding: const EdgeInsets.all(32),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(50),
+                  top: Radius.circular(45),
                 ),
                 boxShadow: [
                   BoxShadow(
                     color: Colors.grey.shade600,
                     offset: const Offset(0, 2),
                   )
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Nome e quantidade
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.itemName,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 30,
+                        width: 70,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+
+                  // Preço
+                  Text(
+                    utilsServices.priceToCurrency(item.price),
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: CustomColors.customSwatchColor,
+                    ),
+                  ),
+
+                  // Descrição
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          item.description,
+                          style: const TextStyle(
+                            height: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Botão de add ao carrinho
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Botão"),
+                  ),
                 ],
               ),
             ),
